77cc5b1b7269d0e933f918c19c41e18573c55440
egbavou <egbavou32@gmail.com>
2022-07-25 03:23:59 +0100

    fix

--- database/seeders/AdminsSeeder.php
+++ database/seeders/AdminsSeeder.php
@@ -17,24 +17,9 @@ class AdminsSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        $company = Company::create([
-            'name' => 'Charnoy',
-            'address' => 'Admin',
-            'city'  => 'Bruxelles',
-            'postal_code' => '1234',
-            'email' => 'admin@admin.com',
-            'vat_number'    => 'BERRR343432'
-        ]);
-    
-=======
-        
->>>>>>> 9e02e8431eb65b27c4c3346bf8f50f2dec0caef7
         $data = [
-
             [
                 'email' => 'admin@facilesolution.com',
-                // 'password' => Hash::make('tXcb_r6[uxXj_9~5'), 
                 'password' => Hash::make('azertyui'),
                 'is_admin'    => true
             ],
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,18 +23,10 @@ class DatabaseSeeder extends Seeder
         $this->call([
             PermissionsSeeder::class,
             AdminsSeeder::class,
-<<<<<<< HEAD
             CategorySeeder::class,
-            DocumentSeeder::class,
+            //DocumentSeeder::class,
             GroupSeeder::class,
             PeopleSeeder::class,
-
-            // EventSeeder::class,
-            // CompanySeeder::class,
-            // OrderSeeder::class,
-            // EventDroitSeeder::class
-=======
->>>>>>> 9e02e8431eb65b27c4c3346bf8f50f2dec0caef7
         ]);
     }
 }
