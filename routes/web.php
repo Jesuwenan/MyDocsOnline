7ba3d9d7b4e879ded9c766fceb92b2449343fd85
A. Floriano D. S. GOMEZ <37933636+SergioTopBest@users.noreply.github.com>
2022-07-26 02:51:50 +0100

    pull

--- routes/web.php
+++ routes/web.php
@@ -38,9 +38,6 @@ use App\Models\Document\DocumentShared;
 
     Route::redirect('dashboard', 'documents');
 
-    Route::resource('participants', ParticipantController::class)
-        ->middleware('auth:sanctum');
-
     Route::resource('documents', DocumentController::class)
         ->middleware('auth:sanctum');
 
@@ -59,57 +56,15 @@ use App\Models\Document\DocumentShared;
     Route::post('share', [DocumentController::class, 'share'])
     ->name('documents.share');
 
-        Route::post('user_share', [DocumentController::class, 'user_share'])
-        ->name('documents.user_share');
+    Route::post('user_share', [DocumentController::class, 'user_share'])
+    ->name('documents.user_share');
 
     Route::get('view_shared_docs', [DocumentController::class, 'view_shared_docs'])
         ->name('documents.view.sharedDocs');
 
     Route::get('view_document_shared', [DocumentController::class, 'share'])
         ->name('documents.share_docs');
-
-    Route::post('/participants/send-mail',[ParticipantController::class, 'sendMail'])
-        ->name('participants.send.mail')
-        ->middleware('auth:sanctum');
-
-    Route::post('/participants/send-sms',[ParticipantController::class, 'sendSms'])
-        ->name('participants.send.sms')->middleware('auth:sanctum');
-
-    Route::post('/participants/send-sms-selected-participants',[ParticipantController::class, 'sendSmsToSelectedParticipants'])
-        ->name('participants.send.sms.selected.participants')
-        ->middleware('auth:sanctum');
-
-    Route::post('/participants/send-mail-selected-participants',[ParticipantController::class, 'sendMailToSelectedParticipants'])
-        ->name('participants.send.mail.selected.participants')->middleware('auth:sanctum');
-
-    Route::get('/confirm-invitation/{id}',[ParticipantController::class, 'showConfirmInviationForm'])
-        ->name('participants.show.confirm.form');
-
-    Route::get('/response',[ResponseController::class, 'Response'])->name('response');
-
-    Route::post('/confirm-invitation',[ParticipantController::class, 'confirmInviation'])
-        ->name('participants.confirm');
-
-    Route::get('/refused-invitation/{id}',[ParticipantController::class, 'showRefusedInviationForm'])
-        ->name('participants.show.refused.form');
-
-    Route::get('/failed',[ResponseController::class, 'refusedResponse'])->name('failed.response');
-
-    Route::post('/refused-invitation',[ParticipantController::class, 'refusedInviation'])
-        ->name('participants.refused');
-
-    Route::get('/condition-general',[ParticipantController::class, 'conditionGeneral'])
-        ->name('participants.condition.general');
-
-    Route::post('/participants/send-ticket',[ParticipantController::class, 'sendTicket'])
-        ->name('participants.send.ticket')->middleware('auth:sanctum');
-
-    Route::post('/participants/send-tickets',[ParticipantController::class, 'sendTickets'])
-        ->name('participants.send.tickets')->middleware('auth:sanctum');
-
-    Route::get('/participants/export/list',[ParticipantController::class, 'exportParticipant'])
-        ->name('participants.export.list')->middleware('auth:sanctum');
-
+        
     Route::resource('people', PersonController::class)->middleware('auth:sanctum');
 
     Route::get('filepond/api/process', [FilepondController::class, 'show']);
